9c3b5900e50771746ad3d355d209f4cfd99e3d37
DanyloBeha <d.beha@ukma.edu.ua>
2026-05-19 09:49:22 +0300

    lab03/cw7: add script for guess number game

---
+++ Lab03/game.js
@@ -0,0 +1,54 @@
+/* 
+Програма випадково обирає число від 1 до 100 (Math.random()).
+Користувач вводить здогад і натискає кнопку.
+Програма відповідає: «менше», «більше», «вгадав!».
+Це загадане число ? наша здогадка
+Максимум 7 спроб. Після 7-ї — поразка з показом числа.
+Лічильник спроб видимий.
+Після вгадування або поразки — кнопка змінюється на «Грати ще раз», починається нова гра.
+Валідація: якщо вводять не число, поза діапазоном 1-100, або не вводять — повідомлення про помилку, спроба НЕ зараховується.
+додайте таблицю рекордів (зберігається в localStorage): після перемоги ім’я + кількість спроб у топ-5.
+*/
+
+var attempts = 7;
+var key = Math.floor(Math.random() * 100);
+
+function restartGame() {
+    document.getElementById("guess").value = "";
+    document.getElementById("submit").textContent = "Грати ще раз";
+}
+
+function guessNumber() {
+
+    if (document.getElementById("submit").textContent == "Грати ще раз") {
+        document.getElementById("submit").textContent = "Перевірити";
+        attempts = 7;
+        document.getElementById("attempts").textContent = attempts;
+        key = Math.floor(Math.random() * 100);
+        return;
+    }
+
+    const guess = document.getElementById("guess").value;
+
+    if (guess < 1 || guess > 100) {
+        alert("Число має бути в діапазоні 1-100");
+        return;
+    }
+
+    if (guess > key) {
+        document.getElementById("message").textContent = "загадане число менше";
+        document.getElementById("attempts").textContent = --attempts;
+    } else if (guess < key) {
+        document.getElementById("message").textContent = "загадане число більше";
+        document.getElementById("attempts").textContent = --attempts;
+    } else {
+        document.getElementById("message").textContent = "вгадав!"
+        restartGame();
+    }   
+
+    if (attempts == 0) {
+        document.getElementById("message").textContent = "здув!";
+        restartGame();
+    }
+
+}
