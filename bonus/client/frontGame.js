aeff2e9ceefabc6423bf2fbaafc01b08133d8c89
DanyloBeha <d.beha@ukma.edu.ua>
2026-06-11 19:41:14 +0300

    bonus: remove report from pivots

--- bonus/client/frontGame.js
+++ bonus/client/frontGame.js
@@ -1,92 +1,12 @@
 // creating the table
 const userBoardPivot = new WebDataRocks({
     container: "#userBoard",
-    toolbar: false,
-    report: {
-        dataSource: {
-            data: []
-        },
-        slice: {
-            rows: [{ uniqueName: "row" }],
-            columns: [{ uniqueName: "col" }],
-            measures: [{ uniqueName: "state", aggregation: "sum" }]
-        },
-        tableSizes: {
-            columns: [
-                {idx: 0, width: 30},
-                {idx: 1, width: 30},
-                {idx: 2, width: 30},
-                {idx: 3, width: 30},
-                {idx: 4, width: 30},
-                {idx: 5, width: 30},
-                {idx: 6, width: 30},
-                {idx: 7, width: 30},
-                {idx: 8, width: 30},
-                {idx: 9, width: 30}
-            ],
-            rows: [
-                {idx: 0, width: 30},
-                {idx: 1, width: 30},
-                {idx: 2, width: 30},
-                {idx: 3, width: 30},
-                {idx: 4, width: 30},
-                {idx: 5, width: 30},
-                {idx: 6, width: 30},
-                {idx: 7, width: 30},
-                {idx: 8, width: 30},
-                {idx: 9, width: 30}
-            ]
-        }
-    }
-    // customizeCell: function(cell, data) {
-    //     cell.style.width = "30px";
-    //     cell.style.height = "30px";
-    // }
+    toolbar: false
 });
 
 const enemyBoardPivot = new WebDataRocks({
     container: "#enemyBoard",
-    toolbar: false,
-    report: {
-        dataSource: {
-            data: []
-        },
-        slice: {
-            rows: [{ uniqueName: "row" }],
-            columns: [{ uniqueName: "col" }],
-            measures: [{ uniqueName: "state", aggregation: "sum" }]
-        },
-        tableSizes: {
-            columns: [
-                {idx: 0, width: 30},
-                {idx: 1, width: 30},
-                {idx: 2, width: 30},
-                {idx: 3, width: 30},
-                {idx: 4, width: 30},
-                {idx: 5, width: 30},
-                {idx: 6, width: 30},
-                {idx: 7, width: 30},
-                {idx: 8, width: 30},
-                {idx: 9, width: 30}
-            ],
-            rows: [
-                {idx: 0, width: 30},
-                {idx: 1, width: 30},
-                {idx: 2, width: 30},
-                {idx: 3, width: 30},
-                {idx: 4, width: 30},
-                {idx: 5, width: 30},
-                {idx: 6, width: 30},
-                {idx: 7, width: 30},
-                {idx: 8, width: 30},
-                {idx: 9, width: 30}
-            ]
-        }
-    }
-    // customizeCell: function(cell, data) {
-    //     cell.style.width = "30px";
-    //     cell.style.height = "30px";
-    // }
+    toolbar: false
 });
 
 userBoardPivot.on('cellclick', function(cell) {
