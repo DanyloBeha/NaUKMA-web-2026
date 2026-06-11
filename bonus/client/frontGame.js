bcc15e35c3d2a9a6653f16c2c6fd5de0f9c0bb24
DanyloBeha <d.beha@ukma.edu.ua>
2026-06-12 02:50:55 +0300

    bonus/client: rroll back from important css styling

--- bonus/client/frontGame.js
+++ bonus/client/frontGame.js
@@ -7,12 +7,14 @@ ID 3 = •
 
 // creating the table
 function customCell(cell, data) {
+    console.log(data.type, data.value);
     if (data.type != 'value') { return; }
-    const val = Number(data.value);
-    if (val < 0 || val > 3) { return; }
-    cell.classList.add('cell-' + val);
-    if (val === 2) { cell.innerHTML = '✘'; }
-    if (val === 3) { cell.innerHTML = '•'; }
+    switch (Number(data.value)) {
+        case 0: cell.style.backgroundColor = 'white'; cell.innerHTML = '';  break;
+        case 1: cell.style.backgroundColor = 'gray'; cell.innerHTML = '';  break;
+        case 2: cell.style.backgroundColor = 'red'; cell.innerHTML = '✘';  break;
+        case 3: cell.style.backgroundColor = 'white'; cell.innerHTML = '•';  break;
+    }
 }
 
 const userBoardPivot = new WebDataRocks({
