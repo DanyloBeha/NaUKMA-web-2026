5c69079ba8d8d1519ee2f3008438a65f0885fa89
DanyloBeha <d.beha@ukma.edu.ua>
2026-06-11 17:51:14 +0300

    bonus: add cells sizes and socket io connection for basic initial rendering

--- bonus/client/frontGame.js
+++ bonus/client/frontGame.js
@@ -1,31 +1,45 @@
-const pivot = new WebDataRocks({
-    container: "#pivotContainer",
+// creating the table
+const userBoardPivot = new WebDataRocks({
+    container: "#userBoard",
     report: {
         dataSource: {
-            filename: ""
+            data: []
         },
-        slice: {
-            rows: [
-                {
-                    filter: {
-                        "type": "top",
-                        quantity: 10,
-                    }
-                }
-            ],
-            columns: [
-                {
-                    filter: {
-                        "type": "top",
-                        quantity: 10,
-                    }
-                }
-            ]
-        }
+    },
+    customizeCell: function(cell, data) {
+        cell.style.width = "30px";
+        cell.style.height = "30px";
     }
 });
 
-webdatarocks.on('cellclick', function(cell) {
+const enemyBoardPivot = new WebDataRocks({
+    container: "#enemyBoard",
+    report: {
+        dataSource: {
+            data: []
+        },
+    },
+    customizeCell: function(cell, data) {
+        cell.style.width = "30px";
+        cell.style.height = "30px";
+    }
+});
+
+userBoardPivot.on('cellclick', function(cell) {
     // cell.rowIndex, cell.columnIndex
-    alert("Click on cell - row: " + cell.rowIndex + ", column: " + cell.columnIndex);
+    alert("Click on home board, row: " + cell.rowIndex + ", column: " + cell.columnIndex);
+});
+
+enemyBoardPivot.on('cellclick', function(cell) {
+    // cell.rowIndex, cell.columnIndex
+    alert("Click on enemy board, row: " + cell.rowIndex + ", column: " + cell.columnIndex);
+});
+
+
+// socket.io connection
+const socket = io('https://naukma-web-2026.onrender.com');
+
+socket.on('boardInit', ({userBoard, enemyBoard}) => {
+    userBoardPivot.updateData({data: userBoard});
+    enemyBoardPivot.updateData({data: enemyBoard});
 });
